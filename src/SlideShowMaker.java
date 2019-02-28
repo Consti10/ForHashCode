8307ae0276890b7ed200ef386a67f8e65d605b6f
Eärendil <earendil>
2019-02-28 20:39:36 +0100

    fixed SlideShow to work with lists

--- src/SlideShowMaker.java
+++ src/SlideShowMaker.java
@@ -1,82 +1,65 @@
 
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SlideShowMaker {
 
 	//Erstellt eine Liste aller gemeinsamen Elemente
-	private String[] calculateCommon(Slide a, Slide b) {
+	private List<String> calculateCommon(Slide a, Slide b) {
 		
 		List<String> ausgabe = new ArrayList<String>(0);
 		
-		for (int index = 0; index < a.tags.length; index++) {
-			for (int index2 = 0; index2 < b.tags.length; ) {
-				if(a.tags[index].equals(b.tags[index2])) {
-					ausgabe.add(a.tags[index]);
+		for (int index = 0; index < a.tags.size(); index++) {
+			for (int index2 = 0; index2 < b.tags.size(); ) {
+				if(a.tags.get(index).equals(b.tags.get(index2))) {
+					ausgabe.add(a.tags.get(index));
 				}
 			}		
 		}
 		
 		
-		return ausgabe.toArray();
-		
-		
+		return ausgabe;
 	}
-	
-	
-	
+
 	private int calculateScore(Slide a, Slide b) {
 		
-		String[] common = calculateCommon(a, b);
-		int anzahlCommon = common.length;
-		
+		List<String> common = calculateCommon(a, b);
+		int anzahlCommon = common.size();
 				
-		int leftJoined = a.tags.length - anzahlCommon;	
+		int leftJoined = a.tags.size() - anzahlCommon;	
 		
-		int rightJoined = b.tags.length - anzahlCommon;
+		int rightJoined = b.tags.size() - anzahlCommon;
 			
 		return Math.min(Math.min(leftJoined, rightJoined), anzahlCommon);
 		
 	}
 	
-	//vertauscht zwei Elemente im array
-	private Slide[] swap(Slide[]eingabe, int index1, int index2) {
+	public List<Slide> buildSlideShow(List<Slide> eingabe) {
 		
-		Slide temp = eingabe[index1];
-		eingabe[index1] = eingabe[index2];
-		eingabe[index2] = temp;
-		
-		return eingabe;
-	}
-	
-	
-	
-	
-	public Slide[] buildSlideShow(Slide[] eingabe) {
-		
-		Slide[] a = eingabe;
-		Slide[] ausgabe = new Slide[a.length];
+		List<Slide> a = eingabe;
+		List<Slide> ausgabe = new ArrayList<>(a.size());
 		
 		int starter = 0;
 		int highestMatch = 0;
 		int pointer = 0;
 		
 		//iteriert
-		for(int index = 0; index < a.length; index++) {
+		for(int index = 0; index < a.size(); index++) {
 			
-			if(index == a.length-1) {
+			if(index == a.size()-1) {
 				break;
 			}
 			
-			for (int index2 = index+1; index2 < a.length; index2++) {
+			for (int index2 = index+1; index2 < a.size(); index2++) {
 				
-				if(calculateScore(a[index], a[index2]) > highestMatch) {
-					highestMatch = calculateScore(a[index], a[index2]);
+				if(calculateScore(a.get(index), a.get(index2)) > highestMatch) {
+					highestMatch = calculateScore(a.get(index), a.get(index2));
 					pointer = index2;
 				}
 			}
-			swap(a, index+1, pointer);
+			Collections.swap(a, index+1, pointer);
 			
 		}
 
